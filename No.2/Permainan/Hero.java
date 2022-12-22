b7eb0381468099e200e62330fe34a82d86820f91
taufikramdann <115464057+taufikramdann@users.noreply.github.com>
2022-12-22 23:51:35 +0700

    Create Hero.java

---
+++ No.2/Permainan/Hero.java
@@ -0,0 +1,9 @@
+package Nusput.P9.No.2.permainan;
+
+public abstract class Hero {
+    abstract String getName();
+    abstract float getHp();
+    abstract float getAttack();
+    abstract float getDefense();
+    public abstract void infoHero();
+}
