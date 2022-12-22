6945e1bf877b49f1d2e495f4f4b11a242de944b4
taufikramdann <115464057+taufikramdann@users.noreply.github.com>
2022-12-22 23:39:42 +0700

    Create Mahasiswa.java

---
+++ No.1/Mahasiswa.java
@@ -0,0 +1,31 @@
+paket  Nusput . P9 . No.1 ;
+
+public  class  Mahasiswa < A , B , C >{
+    pribadi  A  nim ;
+     nama B  pribadi ;
+     kelas C  pribadi ;
+
+    publik  A  getNim (){
+        kembalikan  nim ;
+    }
+
+    public  void  setNim ( A  nim ){
+        ini . nim = nim ;
+    }
+
+    publik  B  getName (){
+        mengembalikan  ini . nama ;
+    }
+
+    public  void  setName ( nama B  ){
+        ini . nama = nama ;
+    }
+
+    publik  C  getClass (){
+         kelas kembali ;
+    }
+
+    public  void  setClass ( C  class ){
+        ini . kelas = kelas ;
+    }
+}
