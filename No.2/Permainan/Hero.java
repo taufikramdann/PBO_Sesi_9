ba7b425c9a6d9f52c124d50dd33c656c8aa14434
taufikramdann <115464057+taufikramdann@users.noreply.github.com>
2022-12-23 08:20:25 +0700

    Update Hero.java

--- No.2/Permainan/Hero.java
+++ No.2/Permainan/Hero.java
@@ -1,9 +1,7 @@
-package Nusput.P9.No.2.Game;
-
 public abstract class Hero {
     abstract String getName();
     abstract float getHp();
     abstract float getAttack();
     abstract float getDefense();
     public abstract void infoHero();
-} 
+}
